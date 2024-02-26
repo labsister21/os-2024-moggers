60e659123cfeb44f3470af960bc9df1176243c95
WazeAzure <bryan.rich0604@gmail.com>
2024-02-27 01:37:03 +0700

    feat: revert libraries to template

--- src/std/string.c
+++ src/std/string.c
@@ -1,45 +1,45 @@
-#include "string.h"
+#include <stdint.h>
+#include <stddef.h>
+#include "std/string.h"
 
-void clear(void *pointer, size_t n) {
-    uint8_t *ptr       = (uint8_t*) pointer;
+void* memset(void *s, int c, size_t n) {
+    uint8_t *buf = (uint8_t*) s;
+    for (size_t i = 0; i < n; i++)
+        buf[i] = (uint8_t) c;
+    return s;
+}
+
+void* memcpy(void* restrict dest, const void* restrict src, size_t n) {
+    uint8_t *dstbuf       = (uint8_t*) dest;
+    const uint8_t *srcbuf = (const uint8_t*) src;
+    for (size_t i = 0; i < n; i++)
+        dstbuf[i] = srcbuf[i];
+    return dstbuf;
+}
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+    const uint8_t *buf1 = (const uint8_t*) s1;
+    const uint8_t *buf2 = (const uint8_t*) s2;
     for (size_t i = 0; i < n; i++) {
-        ptr[i] = 0x00;
+        if (buf1[i] < buf2[i])
+            return -1;
+        else if (buf1[i] > buf2[i])
+            return 1;
     }
+
+    return 0;
 }
 
-size_t strlen(char *string) {
-    size_t i = 0;
-    while (string[i] != '\0')
-        i++;
-    return i;
-}
-
-// 0 sama, 1 beda
-uint8_t strcmp(char *s1, char *s2) {
-    size_t i = 0;
-    if (strlen(s1) == strlen(s2)) {
-        while (s1[i] != '\0') {
-            if (s1[i] != s2[i])
-                return 1;
-            i++;
-        }
-        return 0;
-    }
-    return 1;
-}
-
-void strcpy(char *dst, char *src, int type) {
-    size_t i = 0;
-    clear(dst, strlen(dst));
-    if (type == 1) {
-        while (src[i] != '\0') {
-            dst[i] = src[i];
-            i++;
-        }
+void *memmove(void *dest, const void *src, size_t n) {
+    uint8_t *dstbuf       = (uint8_t*) dest;
+    const uint8_t *srcbuf = (const uint8_t*) src;
+    if (dstbuf < srcbuf) {
+        for (size_t i = 0; i < n; i++)
+            dstbuf[i]   = srcbuf[i];
     } else {
-        while (src[i] != 0xA) {
-            dst[i] = src[i];
-            i++;
-        }
+        for (size_t i = n; i != 0; i--)
+            dstbuf[i-1] = srcbuf[i-1];
     }
+
+    return dest;
 }
--- src/std/string.h
+++ src/std/string.h
@@ -1,15 +1,56 @@
-// Implementasi library pemrosesan string
-#include "stdtype.h"
+#ifndef _STDMEM_H
+#define _STDMEM_H
 
-#ifndef _STRINGH_
-#define _STRINGH_
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 
-void clear(void *pointer, size_t n);
+/**
+ * C standard memset, check man memset or
+ * https://man7.org/linux/man-pages/man3/memset.3.html for more details
+ * 
+ * @param s Pointer to memory area to set
+ * @param c Constant byte value for filling memory area
+ * @param n Memory area size in byte 
+ * 
+ * @return Pointer s
+*/
+void* memset(void *s, int c, size_t n);
 
-size_t strlen(char *string);
+/**
+ * C standard memcpy, check man memcpy or
+ * https://man7.org/linux/man-pages/man3/memcpy.3.html for more details
+ * 
+ * @param dest Starting location for memory area to set
+ * @param src Pointer to source memory
+ * @param n Memory area size in byte 
+ * 
+ * @return Pointer dest
+*/
+void* memcpy(void *restrict dest, const void* restrict src, size_t n);
 
-uint8_t strcmp(char *s1, char *s2);
+/**
+ * C standard memcmp, check man memcmp or
+ * https://man7.org/linux/man-pages/man3/memcmp.3.html for more details
+ * 
+ * @param s1 Pointer to first memory area
+ * @param s2 Pointer to second memory area
+ * @param n Memory area size in byte 
+ * 
+ * @return Integer as error code, zero for equality, non-zero for inequality
+*/
+int memcmp(const void *s1, const void *s2, size_t n);
 
-void strcpy(char *dst, char *src, int type);
+/**
+ * C standard memmove, check man memmove or
+ * https://man7.org/linux/man-pages/man3/memmove.3.html for more details
+ * 
+ * @param dest Pointer to destination memory
+ * @param src Pointer to source memory
+ * @param n Memory area size in byte 
+ * 
+ * @return Pointer dest
+*/
+void *memmove(void *dest, const void *src, size_t n);
 
 #endif
