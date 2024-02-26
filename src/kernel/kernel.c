ddab7211e182ec032130b8bd31ceb8e3b9c43cfa
WazeAzure <bryan.rich0604@gmail.com>
2024-02-27 01:00:44 +0700

    feat: update kernel testing

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,9 +3,15 @@
 #include "gdt/gdt.h"
 #include "kernel/kernel-entrypoint.h"
 
+
 void kernel_setup(void) {
+    /*
     uint32_t a;
     uint32_t volatile b = 0x0000BABE;
     __asm__("mov $0xCAFE0000, %0" : "=r"(a));
     while (TRUE) b += 1;
+    */
+
+    load_gdt(&_gdt_gdtr);
+    while (TRUE);
 }
