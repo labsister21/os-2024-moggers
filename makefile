d937d7262ae836dc3faf44ff74617110cc20efa4
WazeAzure <bryan.rich0604@gmail.com>
2024-03-02 16:09:21 +0700

    RESET - to Chapter 0

--- makefile
+++ makefile
@@ -24,18 +24,12 @@ build: iso
 clean:
 	rm -rf *.o *.iso $(OUTPUT_FOLDER)/kernel
 
-
-
 kernel:
 	@$(ASM) $(AFLAGS) $(SOURCE_FOLDER)/kernel/kernel-entrypoint.s -o $(OUTPUT_FOLDER)/kernel-entrypoint.o
-	@$(ASM) $(AFLAGS) $(SOURCE_FOLDER)/interrupt/intsetup.s -o $(OUTPUT_FOLDER)/intsetup.o
 # TODO: Compile C file with CFLAGS
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/kernel/kernel.c -o $(OUTPUT_FOLDER)/kernel.o
+	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/std/string.c -o $(OUTPUT_FOLDER)/string.o
 	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/gdt/gdt.c -o $(OUTPUT_FOLDER)/gdt.o
-	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/framebuffer/portio.c -o $(OUTPUT_FOLDER)/portio.o
-	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/framebuffer/framebuffer.c -o $(OUTPUT_FOLDER)/framebuffer.o
-	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/interrupt/idt.c -o $(OUTPUT_FOLDER)/idt.o
-	@$(CC) $(CFLAGS) $(SOURCE_FOLDER)/interrupt/interrupt.c -o $(OUTPUT_FOLDER)/interrupt.o
 	@$(LIN) $(LFLAGS) bin/*.o -o $(OUTPUT_FOLDER)/kernel
 	@echo Linking object files and generate elf32...
 	@rm -f *.o
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,7 +2,10 @@
 #include <stdbool.h>
 #include "gdt/gdt.h"
 #include "kernel/kernel-entrypoint.h"
-#include "framebuffer/framebuffer.h"
+// #include "framebuffer/framebuffer.h"
+// #include "interrupt/idt.h"
+// #include "interrupt/interrupt.h"
+// #include "keyboard/keyboard.h"
 
 
 void kernel_setup(void) {
@@ -12,17 +15,33 @@ void kernel_setup(void) {
     __asm__("mov $0xCAFE0000, %0" : "=r"(a));
     while (TRUE) b += 1;
     */
-
-    /*
+   
     load_gdt(&_gdt_gdtr);
     while (true);
-    */
+    
+    // framebuffer_clear();
+    // framebuffer_write(3, 8,  'H', 0, 0xF);
+    // framebuffer_write(3, 9,  'a', 0, 0xF);
+    // framebuffer_write(3, 10, 'i', 0, 0xF);
+    // framebuffer_write(3, 11, '!', 0, 0xF);
+    // framebuffer_set_cursor(3, 10);
+    // while (true);
+    
 
-    framebuffer_clear();
-    framebuffer_write(3, 8,  'H', 0, 0xF);
-    framebuffer_write(3, 9,  'a', 0, 0xF);
-    framebuffer_write(3, 10, 'i', 0, 0xF);
-    framebuffer_write(3, 11, '!', 0, 0xF);
-    framebuffer_set_cursor(3, 10);
-    while (true);
+    // load_gdt(&_gdt_gdtr);
+    // pic_remap();
+    // activate_keyboard_interrupt();
+    // initialize_idt();
+    // framebuffer_clear();
+    // framebuffer_set_cursor(0, 0);
+        
+    // int col = 0;
+    // keyboard_state_activate();
+    // while (true) {
+    //      char c;
+    //      get_keyboard_buffer(&c);
+    //      framebuffer_write(0, col++, c, 0xF, 0);
+    // }
+
+
 }
