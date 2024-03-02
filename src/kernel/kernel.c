aceb415d1df4ece591be65c9c6f9681dc3c3ddc1
WazeAzure <bryan.rich0604@gmail.com>
2024-03-02 11:26:52 +0700

    feat: update kernel testing for IDT

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,6 +3,8 @@
 #include "gdt/gdt.h"
 #include "kernel/kernel-entrypoint.h"
 #include "framebuffer/framebuffer.h"
+#include "interrupt/idt.h"
+#include "interrupt/interrupt.h"
 
 
 void kernel_setup(void) {
@@ -18,11 +20,21 @@ void kernel_setup(void) {
     while (true);
     */
 
+    /*
     framebuffer_clear();
     framebuffer_write(3, 8,  'H', 0, 0xF);
     framebuffer_write(3, 9,  'a', 0, 0xF);
     framebuffer_write(3, 10, 'i', 0, 0xF);
     framebuffer_write(3, 11, '!', 0, 0xF);
     framebuffer_set_cursor(3, 10);
+    while (true);
+    */
+
+    load_gdt(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
+    framebuffer_clear();
+    framebuffer_set_cursor(0, 0);
+    __asm__("int $0x4");
     while (true);
 }
