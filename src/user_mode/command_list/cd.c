a8ce88d3db7b66071512647da84748603feaa949
VansonK <vansonkurnialim@gmail.com>
2024-05-15 00:30:05 +0700

    root fix
    ga bisa mundur lebih dari root

--- src/user_mode/command_list/cd.c
+++ src/user_mode/command_list/cd.c
@@ -13,8 +13,16 @@ void cd(Arg* argument){
 
     // update path
     if(!memcmp(argument->info, "..", 3)){
-        current_working_directory.neff--;
-        memset(&current_working_directory.path[current_working_directory.neff], 0x0, sizeof(current_working_directory.path[current_working_directory.neff]));
+        char root[4] ;
+        root[0] = 'r' ;
+        root[1] = 'o' ;
+        root[2] = 'o' ;
+        root[3] = 't' ;
+        if (memcmp(current_working_directory.path[current_working_directory.neff-1].info, &root, 4)) {
+            current_working_directory.neff--;
+            memset(&current_working_directory.path[current_working_directory.neff], 0x0, sizeof(current_working_directory.path[current_working_directory.neff]));
+        }
+
     } else {
         current_working_directory.path[current_working_directory.neff].len_info = argument->len_info ;
         memcpy(&current_working_directory.path[current_working_directory.neff], argument->info, 8);
